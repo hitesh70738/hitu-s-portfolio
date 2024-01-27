703a3392ea78dd5f4484303f1f17f7a3332e60c6
Hitesh Patel <hitesh@Hiteshs-MacBook-Pro.local>
2024-01-27 14:37:31 +0000

    loaded project details

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -2,6 +2,8 @@ function Projects (props) {
     return (
         <div>
             <h2>{props.title}</h2>
+            <img src={props.image} alt="image" />
+            <br />
             <a href={props.githubLink} target="_blank">Github</a>
             <br />
             <a href={props.deployedLink} target="_blank">View Project</a>
--- portfolio/src/pages/ProjectGallery.jsx
+++ portfolio/src/pages/ProjectGallery.jsx
@@ -1,13 +1,14 @@
 import Project from '../components/Project';
 import ProjectList from '../data/project.json';
 
+
 function ProjectsGallery() {
     // Body
     return <div>
         This is list of my past projects
         <div>
             { ProjectList.map((project) => {
-                return <Project title={project.title} githubLink={project.githubLink} deployedLink={project.deployedLink} />;
+                return <Project title={project.title} githubLink={project.githubLink} deployedLink={project.deployedLink} image={project.image} />;
             })}
         </div>
     </div>
